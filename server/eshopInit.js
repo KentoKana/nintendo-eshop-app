51bd309cf4e099de7a0ac530198bf9a5f96eea60
kentokanazawa@yahoo.com <kentokanazawa@yahoo.com>
2020-01-23 22:01:45 -0500

    removed test HomeController script

--- server/eshopInit.js
+++ server/eshopInit.js
@@ -4,12 +4,11 @@ const EshopApp = require("./App.js");
 const bodyParser = require("body-parser");
 const logger = require("./middlewares/logger");
 
-const HomeController = require("./controllers/HomeController");
 const GamesAmericaController = require("./controllers/GamesAmericaController");
 
 const app = new EshopApp({
   port: 9793,
-  controllers: [new HomeController(), new GamesAmericaController()],
+  controllers: [new GamesAmericaController()],
   middlewares: [
     bodyParser.json(),
     bodyParser.urlencoded({ extended: true }),
